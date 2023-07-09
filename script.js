42ca994029cd6bb6a86fe002166ca2d8e5c93b0d
Nafisa <Nafisafaruna@gmail.com>
2023-07-09 11:51:21 +0100

    feat: edit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) { 
+  event.preventDefault(); //prevent the form from submitting
+  //get form values
+  let name= document.getElementById('name').value;
+  let email= document.getElementById('email').value;
+  let subject= document.getElementById('subject').value;
+  let message= document.getElementById('message').value;
+
+  //Perform actions with form data (e.g send an email, store data e.t.c)
+
+  //reset the form
+   
+  document.getElementById('contact-form').reset();
+})
