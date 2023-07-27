b665b770acad3d7e00b5086a92de58ef229e1598
Nafisa <Nafisafaruna@gmail.com>
2023-07-27 23:15:09 +0100

    feat: done withe the pages

--- script.js
+++ script.js
@@ -14,3 +14,54 @@ document.getElementById("contact-form").addEventListener("submit", function(even
    
   document.getElementById('contact-form').reset();
 })
+
+
+// MOBILE NAVBAR
+
+function toggleMenu(){
+  const hamburger = document.querySelector('.hamburger');
+  const menu = document.getElementById('menu');
+  menu.classList.toggle('active');
+  if (menu.classList.contains("active")){
+    hamburger.innerHTML = `<div class= "bar" style= "transform: rotate(45deg) translate(5px,6px); background-color: #000"></div>
+    <div class= "bar"  style= "opacity: 0; "></div>
+    <div class= "bar"  style= "transform: rotate(-45deg) translate(5px,-6px); background-color: #000;  "></div>
+    `;}
+    else{
+      //change the X icon back to the hamburger  when the menu is inactive
+      hamburger.innerHTML = `<div class="bar"></div>
+      <div class="bar"></div>
+      <div class="bar"></div>`;
+    }
+}
+
+navLinkitems.forEach((link) =>
+ {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+    navLinkitems.forEach((item)=> {
+      item.classList.remove('active');
+    });
+    link.classList.add('active');
+  })
+ }
+
+)
+
+//When the screen is clicked or tapped and the mobile navbar is open it would close
+document.addEventListener('click', function(event) {
+  const navbar = document.querySelector(".nav");
+  const hamburger = document.querySelector('.hamburger');
+  // Check if the clicked element is part of the navbar or if the navbar is closed
+  if (!navbar.contains(event.target) && navbar.classList.contains("active")) {
+    // Close the mobile navbar
+    navbar.classList.remove("active");
+    // If you want to reset the hamburger icon to its initial state (hamburger shape)
+ 
+    hamburger.innerHTML = `
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="bar"></div>
+    `;
+  }
+});
